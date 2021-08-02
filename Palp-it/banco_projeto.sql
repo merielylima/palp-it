13df01af01992c76173c246b1a6f170cd32f1abe
HHugo95 <43177715+HHugo95@users.noreply.github.com>
2021-08-02 20:52:50 -0300

    Delete Palp-it directory

--- Palp-it/banco_projeto.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuario ( 
-id_usuario int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-nome varchar(30) DEFAULT NULL, 
-email varchar(40) DEFAULT NULL, 
-profissao varchar(30) DEFAULT NULL, 
-senha varchar(32) DEFAULT NULL, 
-area varchar (30) DEFAULT NULL)
-ENGINE=InnoDB DEFAULT CHARSET=utf8
--- Palp-it/sair.php
+++
@@ -1,5 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION['id_usuario']); //destruindo a sessao
-  header("location: index.php");//encaminhado para index
- ?>
