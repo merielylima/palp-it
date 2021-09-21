6eed218c0969ac617e39da84e1833c5e6d787520
Meriely Lima <meriellylima96@gmail.com>
2021-09-21 20:38:44 -0300

    alterações
    mudança na pasta assets e alteração de alguns conteúdos de HTML

--- palpit/cadastrar.php
+++ palpit/cadastrar.php
@@ -28,33 +28,36 @@ if(isset($_POST['nome'])){
 						$pdo->commit(); //envia uma transação
 						?>
 							<!DOCTYPE html>
-							<html lang="pt">
-							<head>
-							<meta charset="UTF-8">
-								<meta name="viewport" content="width=device-width, initial-scale=1.0">
-								<title>Palp-it</title>
-								<link rel="preconnect" href="https://fonts.googleapis.com">
-								<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-								<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet">
-								<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"> 
-								<link rel="stylesheet" href="./assets/css/base/base.css">
-								<link rel="stylesheet" href="./assets/css/componentes/cartao.css">
-								<link rel="stylesheet" href="./assets/css/componentes/inputs.css">
-								<link rel="stylesheet" href="./assets/css/componentes/botao.css">
-							</head>
-							<body>
-								<main class="container flex flex--coluna flex--centro">
-									<section class="cartao--container cartao--pequeno">
-										<div class="">
-											<img src="assets/img/Logo-palp-it.svg" alt="Logo Palp-it"/>
-											<h2 class="container--titulo">Falta pouco...</h2>
+								<html lang="pt">
+								<head>
+									<meta charset="UTF-8">
+									<meta name="viewport" content="width=device-width, initial-scale=1.0">
+									<title>Palp-it</title>
+									<link rel="preconnect" href="https://fonts.googleapis.com">
+									<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+									<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet">
+									<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"> 
+									<link rel="stylesheet" href="./assets/css/base/base.css">
+									<link rel="stylesheet" href="./assets/css/componentes/cartao.css">
+									<link rel="stylesheet" href="./assets/css/componentes/inputs.css">
+									<link rel="stylesheet" href="./assets/css/componentes/botao.css">
+								</head>
+								<body>
+									<main class="container container-xs">
+										<div class="content flex flex--centro flex--coluna mt7">
+											<section class=" cartao--container cartao-xs">
+												<div class="border-bottom">
+													<img src="assets/img/Logo-palp-it.svg" alt="Logo Palp-it"/>
+													<h2 class="container--titulo">Falta pouco...</h2>
+												</div>
+												<p class="justificado  my-3">  Para concluir seu cadastro, é necessário clicar no link de confirmação enviado para o endereço <a class="link" > <?php echo $email ?> </a>. </p>
+												<a href="inicio_cadastro.html"> <button class="botao--container botao--primario width-full">  Voltar para página inicial </button></a>
+											</section>
 										</div>
-										<p class="mensagem">  Para concluir seu cadastro, é necessário clicar no link de confirmação enviado para o endereço <a class="link mensagem" > <?php echo $email ?> </a>. </p>
-										<a href="inicio_cadastro.php"> <button class="botao--container botao--primario botao--block">  Voltar para página inicial </button></a>
-									</section>
-								</main>
-							</body>
-							</html>
+									</main>
+								</body>
+								</html>
+
 						<?php
 					} 
 					else {
