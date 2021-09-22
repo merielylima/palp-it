d90bc226b5e3a9a0ae1b8d8ec28e73a792396aa3
HHugo95 <hugo.hslima@gmail.com>
2021-09-21 23:34:18 -0300

    Contribuição Alterado

---
+++ palpit/contribuicoes_alterado.php
@@ -0,0 +1,17 @@
+<?php
+ include ("cabecalho.php");
+?>
+
+  <main class="container flex flex--centro">
+
+    <nav class="cartao__perfil">
+      
+    </nav>
+    <section class="cartao">
+
+    </section>
+  </main>
+
+<?php
+include ("rodape.php");
+?>
