5004a187139d38ba5ba5dd0c216522fbb405c8ef
HHugo95 <hugo.hslima@gmail.com>
2022-02-25 11:41:24 -0300

    Update
    Formulario.js - remoção readURL width e height

--- palpit/js/formulario.js
+++ palpit/js/formulario.js
@@ -62,9 +62,7 @@ function readURL(input) {
         var reader = new FileReader ();
         reader.onload = function (e) {
         $ ('#img_prev')
-            .attr ('src', e.target.result)
-            .width (75)
-            .height (100);
+            .attr ('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
     }
