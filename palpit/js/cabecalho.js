5b41a13d2447516018f795389816a6be043a6bc4
Meriely Lima <meriellylima96@gmail.com>
2022-03-03 20:35:20 -0300

    funções de active das paginas

--- palpit/js/cabecalho.js
+++ palpit/js/cabecalho.js
@@ -1,26 +1,3 @@
-var btnInicio = document.querySelector("#btn-inicio");
-var btnPerfil = document.querySelector("#btn-perfil");
-var btnContribuicoes = document.querySelector("#btn-contribuicoes");
-
-
-btnInicio.addEventListener('click',() =>{
-    btnInicio.classList.add("active");
-    btnPerfil.classList.remove("active");
-    btnContribuicoes.classList.remove("active");
-})
-
-btnPerfil.addEventListener('click',() =>{
-    btnInicio.classList.remove("active");
-    btnPerfil.classList.add("active");
-    btnContribuicoes.classList.remove("active");
-})
-
-btnContribuicoes.addEventListener('click',() =>{
-    btnInicio.classList.remove("active");
-    btnPerfil.classList.remove("active");
-    btnContribuicoes.classList.add("active");
-})
-
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
